e4c6143b02cd27597185a3fef9490ee43aa79089
Eros Santos de Vasconcelos <101269652+ErosVasconcelos@users.noreply.github.com>
2024-05-13 11:43:26 -0300

    Update Menu.tsx

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import React, { useState } from 'react';
 import Head from 'next/head';
-import ControleEditora from '../classes/controle/ControleEditora';
 import { useRouter } from 'next/router';
 import { Card, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
